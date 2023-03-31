66d253291711c801860a6b760beffae01880cea6
Shu <controvictor@gmail.com>
2023-03-31 03:36:07 -0300

    atualizando o projeto com expressoes lambda e api stream

--- src/desafio_Logica_1/Program.java
+++ src/desafio_Logica_1/Program.java
@@ -4,46 +4,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-
-		List<Integer> list = new ArrayList<Integer>();
-		List<Integer> listPar = new ArrayList<Integer>();
-		List<Integer> listImpar = new ArrayList<Integer>();
-
+		List<Integer> list = new ArrayList<>();
 		System.out.print("quantos numeros irá digita? ");
-		int line = scan.nextInt();
+		int num = scan.nextInt();
 
-		for (int i = 0; i < line; i++) {
+		for (int i = 0; i < num; i++) {
+			System.out.print("digite um numero:  ");
 			list.add(scan.nextInt());
-		}
-
-		for (int i : list) {
-			if (i % 2 == 0) {
-				listPar.add(i);
-			}
 
 		}
-		for (int i : list) {
-			if (i % 2 == 1) {
-				listImpar.add(i);
-			}
 
-		}
-		Collections.sort(listPar);
-		Collections.sort(listImpar,Collections.reverseOrder());
-		
-		
-		for (int i : listPar) {
-			System.out.println(i);
-		}
-		for (int i : listImpar) {
-			System.out.println(i);
-		}
+		List<Integer> listPar = list.stream().filter(p -> p % 2 == 0).collect(Collectors.toList());
+		List<Integer> listImpar = list.stream().filter(i -> i % 2 == 1).collect(Collectors.toList());
+
+		Collections.sort(listImpar, Collections.reverseOrder());
+		System.out.println();
+		System.out.println("Números Pares");
+		listPar.stream().forEach(p -> System.out.println(p));
+		System.out.println("Números Impares");
+		listImpar.stream().forEach(i -> System.out.println(i));
 
 		scan.close();
 	}
